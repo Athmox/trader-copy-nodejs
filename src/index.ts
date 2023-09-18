c26c6309f57e1659ac0101442fe5b6064d6030a5
Markus Scheider <scheider.markus00@gmail.com>
2023-09-18 09:04:58 +0200

    code cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,11 @@ import 'dotenv/config';
 import 'module-alias/register';
 import validateEnv from '@/utils/validateEnv';
 import App from './app';
-import PostController from '@/resources/post/post.controller';
-import UserController from '@/resources/user/user.controller';
 import GmxController from '@/resources/controller/gmx.controller';
 validateEnv();
 
 const app = new App(
-    [new PostController(), new UserController(), new GmxController()],
+    [new GmxController()],
     Number(process.env.PORT)
 );
 
